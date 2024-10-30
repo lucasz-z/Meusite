fa618cf4e85f13cf66dd25df6304982fe8ece854
lucasz-z <lucaszambon123@gmail.com>
2024-10-29 20:02:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,45 +22,58 @@ def main(pagina: ft.Page):
     pagina.title = "Consulta de Códigos - Blindagem"
     pagina.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     pagina.vertical_alignment = ft.MainAxisAlignment.CENTER
-    
+    pagina.expand = True
+
+    barra_visivel = False
+
     # Carregar a planilha com os dados
     df = carregar_dados()
     
     # Lista de códigos para o autocomplete
     codigos_sugestoes = df["CÓDIGOS NOVO"].tolist()
 
-    #Funçao para mudar conteudo
+    #função para alterar a visibilidade da aba
+    def alterar_barra(e):
+        nonlocal barra_visivel
+        barra_visivel = not barra_visivel
+        atualizar_layout()
 
+
+    conteudo = ft.Column(expand=True)
+
+    #Funçao para mudar conteudo
     def mudar_aba(e):
         conteudo.controls.clear()
         if e.control.selected_index == 0:
             conteudo.controls.append(Blindagem())
         elif e.control.selected_index == 1:
-            conteudo.controls.append(ft.Text("% avalidar"))
+            conteudo.controls.append(ft.Text("em breve"))
         pagina.update()
 
     #função de contrução
     def Blindagem():
+
+        pagina_pequena = pagina.width <= 600
             
         # Campo de entrada para o código
-        codigo_input = ft.TextField(label="Insira o Código (múltiplos códigos separados por vírgula)",width=400, border_color= ft.colors.BLUE, hint_text= "digite o codigo aqui")
+        codigo_input = ft.TextField(label="Insira o Código (múltiplos códigos separados por vírgula)",width=pagina.width * 0.9 if pagina_pequena else 400, border_color= ft.colors.BLUE, hint_text= "digite o codigo aqui", border_radius=10)
         
         # Lista de sugestões que será exibida
         sugestoes_lista = ft.Column()
 
         # Campo para exibir os resultados
-        resultado_texto = ft.Column(scroll=ft.ScrollMode.AUTO)
+        resultado_texto = ft.Column(scroll=ft.ScrollMode.ADAPTIVE, height=450, expand=True)
+        pagina.update
+
 
         # Função para sugerir códigos com base no input do usuário
         def sugerir_codigos(e):
-            input_valor = e.control.value.upper() # Texto digitado
-            pagina.update()
+            input_valor = codigo_input.value.upper() # Texto digitado
             
             # Limpa a lista de sugestões
             sugestoes_lista.controls.clear()
 
             # Adiciona códigos compatíveis com o texto digitado
-            input_valor = codigo_input.value.upper()
             for codigo in codigos_sugestoes:
                 if codigo.upper().startswith(input_valor):
                     sugestoes_lista.controls.append(
@@ -87,33 +100,43 @@ def main(pagina: ft.Page):
         
         # Função para ser chamada ao clicar no botão "Consultar"
         def consultar_codigo(e):
-            codigo = codigo_input.value.split(",")
+            codigo = [c.strip().upper() for c in codigo_input.value.split(",")]
             resultado_texto.controls.clear()
 
             if len(codigo) == 0 or codigo ==[""]:  # Validação para verificar se o campo não está vazio
                 resultado_texto.controls.append(ft.Text("Por favor, insira um código.", color="red"))
             else:
                 for codigo in codigo:
-                    codigo = codigo.strip() #remove espaços
                     if codigo == "":
                         continue #pula se estiver vazio
                     
                     #consulta indivual
-                    informacao = buscar_informacoes(df, codigo)
+                    informacao = buscar_informacoes(df, codigo.upper())
                     if informacao is not None:
-                        # Adiciona as informações do código na interface
-                        resultado_texto.controls.append(ft.Text(f"Código: {informacao['CÓDIGOS NOVO']}", color= "black"))
-                        resultado_texto.controls.append(ft.Text(f"Descrição do Código: {informacao['Descrição']}"))
-                        resultado_texto.controls.append(ft.Text(f"Blindagem: {informacao['Blindagem']}"))
-                        resultado_texto.controls.append(ft.Text(f"Causas BT: {informacao['Causas BT']}"))
-                        resultado_texto.controls.append(ft.Text(f"Causas BT e MT: {informacao['Causas BT e MT']}"))
-                        resultado_texto.controls.append(ft.Divider())
+                        resultado_texto.controls.append(
+                            ft.Container(
+                                content=ft.Column([
+                                    ft.Row([ft.Icon(ft.icons.CHECK_CIRCLE, color = ft.colors.GREEN), ft.Text(f"Código: {informacao["CÓDIGOS NOVO"]}")]),
+                                    ft.Text(f"Descrição do Código: {informacao['Descrição']}"),
+                                    ft.Text(f"Blindagem: {informacao['Blindagem']}"),
+                                    ft.Text(f"Causas BT: {informacao["Causas BT"]}"),
+                                    ft.Text(f"Causas BT e MT: {informacao['Causas BT e MT']}"),
+                                    ft.Text(f"Codigo Antigo: {informacao['COD Antigo']}")
+                                ]),
+                                bgcolor = ft.colors.WHITE, border = ft.border.all(1, ft.colors.GREEN_ACCENT), padding=10, margin = 5, border_radius = 8, shadow = ft.BoxShadow(blur_radius=5, spread_radius=1)
+                                        
+                            )
+                        )
                     else:
-                        resultado_texto.controls.append(ft.Text("Codigo não encontrado, por favor verifique o *S* Maiusculo inicial ou *-* entre o codigo", color="red"))
-
-                #Atualiza a página com os resultados
+                        resultado_texto.controls.append(
+                            ft.Container(
+                                content=ft.Row([
+                                    ft.Icon(ft.icons.ERROR, color=ft.colors.RED_ACCENT),
+                                    ft.Text(f"Código {codigo} não encontrado.", color="red")
+                                ]), border= ft.border.all(1, ft.colors.RED_ACCENT), padding=10, margin=5, border_radius=8, shadow= ft.BoxShadow(blur_radius=5, spread_radius=1)
+                            )
+                        )
                 pagina.update()
-
         # função botão limpar
         
         def limpar_input(e):
@@ -122,46 +145,49 @@ def main(pagina: ft.Page):
             resultado_texto.controls.clear()
             pagina.update()
         
-        
+        botao_limpar = ft.ElevatedButton("Limpar", on_click=limpar_input, icon= ft.icons.CLEAR_ALL, bgcolor=ft.colors.RED_ACCENT_100)
+
+        botao_consulta = ft.ElevatedButton("Consultar", on_click=consultar_codigo, icon= ft.icons.SEARCH, bgcolor=ft.colors.GREEN_ACCENT)
         
         #atribui a função de susgest
         codigo_input.on_change = sugerir_codigos
 
+        #retorna o componente conforme o tamanho da tela
         return ft.Column([
-            ft.Row(
-                controls=[codigo_input,
-                ft.ElevatedButton( "limpar", on_click = limpar_input, icon= ft.icons.CLEAR, bgcolor= ft.colors.RED_ACCENT_100),
-                ], alignment= ft.MainAxisAlignment.CENTER,
-            
-            ),
-            ft.ElevatedButton("consultar",on_click= consultar_codigo, icon= ft.icons.SEARCH, bgcolor= ft.colors.WHITE10),
-            sugestoes_lista,
-            resultado_texto], spacing=20, alignment= ft.MainAxisAlignment.CENTER)
+            codigo_input, botao_limpar, botao_consulta,sugestoes_lista,
+            resultado_texto], spacing=25, alignment= ft.MainAxisAlignment.CENTER)
 
-    #barra lateral
-    rail = ft.NavigationRail(
-        selected_index=0,
-        label_type= ft.NavigationRailLabelType.ALL,
-        group_alignment= -0.9,
-        destinations=[
-            ft.NavigationRailDestination(icon=ft.icons.SEARCH_ROUNDED, selected_icon=ft.icons.SEARCH, label="Consulta de Códigos"),
-            ft.NavigationRailDestination(icon=ft.icons.PERCENT_ROUNDED, selected_icon= ft.icons.PERCENT,label = "% avalidar"),
-        ],
-        on_change= mudar_aba
-    )
-    #aba que sera mudada
-    conteudo = ft.Column(expand=True)
-    conteudo.controls.append(Blindagem())
+    def atualizar_layout():
+        pagina.controls.clear()
+        botao_menu = ft.IconButton(icon=ft.icons.MENU, on_click=alterar_barra)
 
-    # Adiciona os componentes na página
-    pagina.add(
-        ft.Row(
-            [rail,
-            ft.VerticalDivider(width=1),
-            conteudo],
-            expand=True
+        #barra lateral
+        rail = ft.NavigationRail(
+            selected_index=0,
+            label_type= ft.NavigationRailLabelType.ALL,
+            group_alignment= -1.0, indicator_color= "blue",
+            destinations=[
+                ft.NavigationRailDestination(icon=ft.icons.SEARCH_ROUNDED, selected_icon=ft.icons.SEARCH, label="Consulta de Códigos"),
+                ft.NavigationRailDestination(icon=ft.icons.PERCENT_ROUNDED, selected_icon= ft.icons.PERCENT,label = "Em breve"),
+            ],
+            on_change= lambda e: mudar_aba(e),
+            visible= barra_visivel
         )
-    )
+        #aba que sera mudada
+        conteudo = ft.Column(expand=True)
+        conteudo.controls.append(Blindagem())
+
+        # Adiciona os componentes na página
+        pagina.add(
+            ft.Row(
+                [rail,
+                ft.VerticalDivider(width=1),
+                conteudo] if barra_visivel else [botao_menu, conteudo],expand=True
+            )
+        )
+        pagina.update()
+    atualizar_layout()
+
 
 # Executa a aplicação
 ft.app(target=main)
